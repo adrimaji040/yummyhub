a08d8a948c841069e201f909478337701ff258f6
Nguyen Dung <vo000086@algonquinlive.com>
2025-08-08 23:57:23 -0400

    Lasted updated v2

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -16,16 +16,13 @@ import { red } from "@mui/material/colors";
 const Card = ({ recipe }) => {
   const { id, cover_photo_url, title, cooking_time, rating, user_name, user_id, } =
     recipe;
-  {
-    console.log("Recipe:", recipe);
-  }
+
   return (
     <MUICard sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
       {console.log(recipe)}
 
       <CardActionArea>
         <CardHeader
-<<<<<<< HEAD
           avatar={
             user_id ? (
               <Link
@@ -44,30 +41,16 @@ const Card = ({ recipe }) => {
               </Avatar>
             )
           }
-=======
->>>>>>> 761118f79a13d05fa3f313b2fe8d36fb3ae09640
           title={
              <Link
               to={`/recipe/${id}`}
               onClick={(e) => e.stopPropagation()}
               style={{ textDecoration: "none", color: "inherit" }}
             >
-              {title}
+              {recipe.title}
             </Link>
           }
-          subheader={
-            user_name && user_id ? (
-              <Link
-                to={`/user/${user_id}`}
-                onClick={(e) => e.stopPropagation()}
-                style={{ textDecoration: "none", color: "inherit" }}
-                title={`View ${user_name}'s profile`}
-              >
-                By {user_name}
-              </Link>
-            ) : null
-            }
-          />
+        />
 
         <Link
           to={`/recipe/${id}`}
